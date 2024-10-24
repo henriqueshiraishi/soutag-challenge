16ea3ed67650621b3123d6a2d5be7e864c4accfd
Henrique <henriqueashiraishi@gmail.com>
2024-10-24 13:23:12 -0300

    Add discount birthday and category methods and change specs

--- price_service.rb
+++ price_service.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class PriceService
   attr_reader :product, :user
 
@@ -6,16 +8,16 @@ class PriceService
     @user = user
   end
 
-  # Método de chamada para recuperar preço final do produto
+  # Método de chamada para recuperar preço final do produto com desconto
   def call
-    final_price
+    final_price - discount_amount
   end
 
   private
 
     # Captura preço base do produto
     def base_price
-      product[:base_price]
+      product[:base_price].to_f
     end
 
     # Calcula preço final do produto
@@ -23,8 +25,35 @@ class PriceService
       base_price + tax_amount
     end
 
-    # Calcula o valor do imposto sobre o preço base do produto
+    # Calcula o valor do imposto sobre o preço base
     def tax_amount
-      base_price * (product[:tax_percentage] / 100.0)
+      base_price * (product[:tax_percentage].to_f / 100.0)
+    end
+
+    # Calcula o total de desconto do produto
+    def discount_amount
+      birthday_discount + category_discount
+    end
+
+    # Calcula o valor do desconto de aniversário sobre o preço final
+    def birthday_discount
+      discount_percentage = 10
+
+      if user[:birthday_month] == Date.today.month
+        final_price * (discount_percentage.to_f / 100)
+      else
+        0
+      end
+    end
+
+    # Calcula o valor do desconto de categoria sobre o preço final
+    def category_discount
+      discount_percentage = 5
+
+      if %w( food beverages ).include?(product[:category])
+        final_price * (discount_percentage.to_f / 100)
+      else
+        0
+      end
     end
 end
--- price_service_spec.rb
+++ price_service_spec.rb
@@ -1,11 +1,12 @@
+require 'date'
 require 'rspec'
 require_relative 'price_service'
 
 RSpec.describe PriceService do
   subject(:call) { PriceService.new(product: product, user: user).call }
 
-  let(:product) { { id: 1, base_price: 100, tax_percentage: 0 } }
-  let(:user) { { id: 1, birthday_month: 5 } }
+  let(:product) { { id: 1, base_price: 100 } }
+  let(:user) { { id: 1, birthday_month: Date.today.next_month.month } }
 
   it 'calculates the total price' do
     expect(call).to eq(100.0)
@@ -14,8 +15,53 @@ RSpec.describe PriceService do
   context 'when product has tax' do
     let(:product) { { id: 1, base_price: 100, tax_percentage: 10 } }
 
-    it 'ignores the taxes' do
+    it 'applies tax amount' do
       expect(call).to eq(110.0)
+    end
+
+    it 'applies category discount' do
+      product[:category] = 'food'
+      expect(call).to eq(104.5)
+    end
+
+    it 'applies birthday discount' do
+      user[:birthday_month] = Date.today.month
+      expect(call).to eq(99)
+    end
+
+    it 'applies category and birthday discount' do
+      product[:category] = 'beverages'
+      user[:birthday_month] = Date.today.month
+      expect(call).to eq(93.5)
+    end
+  end
+
+  context 'when product has category discount' do
+    it 'applies food discount' do
+      product[:category] = 'food'
+      expect(call).to eq(95)
+    end
+
+    it 'applies beverages discount' do
+      product[:category] = 'beverages'
+      expect(call).to eq(95)
+    end
+
+    it 'ignores others category' do
+      product[:category] = 'others'
+      expect(call).to eq(100)
+    end
+  end
+
+  context 'when product has birthday discount' do
+    it 'applies discount amount' do
+      user[:birthday_month] = Date.today.month
+      expect(call).to eq(90)
+    end
+
+    it 'ignores other birthday month' do
+      user[:birthday_month] = Date.today.next_month.month
+      expect(call).to eq(100)
     end
   end
 end
